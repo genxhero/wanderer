31d4ce3d8a5fa3e6697f4e3fb0a86a36a03864df
Rola1993 <rola.uiuc@gmail.com>
2018-10-20 13:56:24 -0700

    set up mangoose

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world"))
+app.get("/", (req, res) => res.send("Hell world"))
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
