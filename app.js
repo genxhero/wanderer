43083e20f9b1e9e877bf7566311f36b73eac646a
Rola1993 <rola.uiuc@gmail.com>
2018-10-20 21:34:17 -0700

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hell world"))
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
--- config/key.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://<dbuser>:<dbpassword>@ds237563.mlab.com:37563/wayfarer"
-  //Make sure this is your own unique string
-};
