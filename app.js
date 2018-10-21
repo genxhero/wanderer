dcf607350291478730558dba5e5427275078681b
Rola1993 <rola.uiuc@gmail.com>
2018-10-20 21:59:53 -0700

    basic setup

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const events = require("./routes/api/events");
+const bodyParser = require("body-parser");
+
 mongoose
   .connect(db)
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+app.use("/api/users", users);
+app.use("/api/events", events);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hell world"))
 
