aeadf7365c774c2c387401f87360c1f310690299
Cassandra McClure <uncertainkitten@gmail.com>
2018-10-21 16:59:36 -0700

    Create login route - add jwt to login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,33 +2,92 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
-
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // We'll validate by email, though I might also add a username validation later
 
 router.post('/register', (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
+  // const { errors, isValid } = validateRegisterInput(req.body);
+
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
+
+  User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "A user has already registered with that email"})
+      errors.name = "A user has already registered with that email";
+      return res.status(400).json(errors);
     } else {
       const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
-      })
+      });
+
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if(err) throw err;
+          if (err) throw err;
           newUser.password = hash;
           newUser.save()
-            .then(user => res.json(user))
+            .then(user => {
+              const payload = {id: user.id, name: user.username};
+
+              jwt.sign(payload, keys.secretOrPrivateKey, { expiresIn: 3600 }, (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              });
+            })
             .catch(err => console.log(err));
-          })
-        })
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+  // const {errors, isValid} = validateLoginInput(req.body);
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({username})
+    .then(user => {
+      if (!user) {
+        errors.username = "This user does not exist"
+        return res.status(400).json(errors);
       }
-    })
-  })
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+         if (isMatch) {
+          const payload = {id: user.id, name: user.username};
+
+          // using jwt per the docs
+          jwt.sign(
+            payload,
+            keys.secretOrPrivateKey,
+            // tells the key to expire in one hour
+
+            {expiresIn: 3600},
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+            });
+          });
+      } else {
+        errors.password = "Incorrect password"
+        return res.status(400).json(errors)
+      }
+    });
+  });
+});
 
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
