43b1c4a427016cd76e5929dbdb07b7f59f829108
genxhero <genxhero@users.noreply.github.com>
2018-10-21 21:35:44 -0700

    Style signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,23 +6,46 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+      <div className="signup-page">
+            <div className="signup-container">
+               <form className="signup-form">
+                 <h1 className="signup-heading">Sign Up</h1>
+                 <h3>Your Information</h3>
+                 <h3>Vehicle Information</h3>
+                 <div className="signup-bottom">
+                 <div id="excite">Adventure Awaits!</div>
+                 <input 
+                  type="submit" 
+                  value= "Allons-y!"
+                  className="signup-submit"
+                  >
+                 </input>
+                 </div>
+               </form>
+            </div>
+          </div>
+      
+
       </div>
     );
   }
 }
 
 export default App;
+
+// 
+// <header className="App-header">
+ // <img src={logo} className="App-logo" alt="logo" />
+//<p>
+ // Edit <code>src/App.js</code> and save to reload.
+//</p>
+//<a
+ // className="App-link"
+  //href="https://reactjs.org"
+  //target="_blank"
+ // rel="noopener noreferrer"
+//>
+  //Learn React
+//</a>
+ //</header>
