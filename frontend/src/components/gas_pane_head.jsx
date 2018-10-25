51b6fffa1c4cd9542a5a84d2b65e5bc745969e02
Aaron Goddard <aaronbear@gmail.com>
2018-10-25 09:10:21 -0700

    Style the front pages

--- frontend/src/components/gas_pane_head.jsx
+++ frontend/src/components/gas_pane_head.jsx
@@ -1,43 +1,37 @@
 import React from 'react';
 
 class GasPaneHead extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+          foodSelected: false,
+          hotelSelected: false,
+          barSelected: false
+        }
+    }
     
     render() {
-    return (
-      <div className="gas-pane-head">
-       
+    return <div className="gas-pane-head">
         <div className="gas-pane-form-container">
-           <form className="gas-pane-form"> 
-              <div className="gas-pane-input">
-                  <input 
-                    className="gas-pane-input-field"
-                    type="text"
-                    placeholder = "Where are you starting from?"
-                    >
-                  </input>
-                  <input 
-                    className="gas-pane-input-field"
-                    type="text"
-                    placeholder="Percentage of full tank"
-                    >
-                  </input>
+          <form className="gas-pane-form">
+            <div className="gas-pane-input">
+              <input id="start-loc"className="gas-pane-input-field" type="text" placeholder="Where to??" />
+              <input id="tank-percent" className="gas-pane-input-field" type="text" placeholder="Percent Full" />
+             
             </div>
 
             <div className="gas-pane-checkboxes">
-                   
+
             </div>
-                <input
-                className ="gas-pane-submit"
-                type="submit"
-                >
-                
-                </input>
-
-            </form>
+            <div className="button-div">
+              <input className="gas-pane-submit" type="submit" value="Do It!!" />
+              <div className="gas-pane-text-bg" />
+              <div className="gas-pane-submit-shadow" />
+            </div>
+          </form>
         </div>
-
-      </div>
-    )
+      </div>;
   }
 }
 
