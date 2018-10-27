2d44372e3b3c7765ea2d129ea0edbff15b213d01
Cassandra McClure <uncertainkitten@gmail.com>
2018-10-26 20:22:55 -0700

    Add button to gas pane head that successfully grabs latitude and longitude of destination

--- frontend/src/components/gas_pane_head.jsx
+++ frontend/src/components/gas_pane_head.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
 import {connect} from 'react-redux';
 import {receiveMapData} from '../util/map_util';
 
@@ -26,6 +30,7 @@ class GasPaneHead extends React.Component {
           }
         }
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleAddress = this.handleAddress.bind(this);
     }
 
     // calculateMaxDistance() {
@@ -54,6 +59,12 @@ class GasPaneHead extends React.Component {
       }
     }
 
+    handleAddress(latLng) {
+      let newForm = Object.assign({}, this.state.formData);
+      newForm.address = latLng;
+      this.setState({formData: newForm});
+    }
+
     // all conversions are done in handleSubmit because the helper functions weren't working
     handleSubmit(e) {
       e.preventDefault();
@@ -75,9 +86,9 @@ class GasPaneHead extends React.Component {
         <div className="gas-pane-form-container">
           <form className="gas-pane-form" onSubmit={this.handleSubmit}>
             <div className="gas-pane-input">
-              <input id="start-loc" className="gas-pane-input-field" placeholder="Where to??" />
+              <LocationSearchInput handleAddress={this.handleAddress} />
               <div className="percent-container">
-                <span className="percent-label">How full is you tank?</span>
+                <span className="percent-label">How full is your tank?</span>
                 <span className="percent-sign">%</span>
               <input id="tank-percent" className="gas-pane-input-field" placeholder="100" value={this.state.formData.percentFull} onChange={this.update("percentFull")} />
               </div>
@@ -105,3 +116,64 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(GasPaneHead);
+
+class LocationSearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { address: "" };
+  }
+
+  handleChange = address => {
+    this.setState({ address });
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => this.props.handleAddress(latLng))
+      .catch(error => console.error("Error", error));
+  };
+
+  render() {
+    return (
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: "Search Places ...",
+                className: "location-search-input"
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? "suggestion-item--active"
+                  : "suggestion-item";
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                  : { backgroundColor: "#ffffff", cursor: "pointer" };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
+  }
+}
