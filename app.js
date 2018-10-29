14a3515d40df11c3b17b11910c8634ecd2ab44cf
Cassandra McClure <uncertainkitten@gmail.com>
2018-10-28 23:17:06 -0700

    Update paths to accommodate folder name change

--- app.js
+++ app.js
@@ -22,18 +22,17 @@ app.use(bodyParser.json());
 
 app.use(passport.initialize());
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 app.get('/', (req, res) => res.send("Welcome to Wayfarer's temp page"));
 app.use("/api/users", users);
 app.use("/api/vehicles", vehicles)
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
