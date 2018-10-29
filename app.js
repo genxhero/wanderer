9293d141c72ff64890d787eeaf326a6923d8dd4e
Cassandra McClure <uncertainkitten@gmail.com>
2018-10-28 23:39:49 -0700

    Hope heroku doesn't break now that I'm reconnecting the frontend

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
