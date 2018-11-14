c392e489db7568b60863e015820b2004bb3c4919
Aaron Goddard <aaronbear@gmail.com>
2018-11-14 10:53:57 -0800

    Add carouselling feature to splash page

--- client/src/components/splash.js
+++ client/src/components/splash.js
@@ -6,7 +6,41 @@ const mapStateToProps = state => ({
   currentUser: state.session
 });
 
+const SplashWelcome = () => (
+  <div className="splash-container">
+    <h1 className="splash-title"> Welcome to Wayfarer!</h1>
+    <span className="splash-info">Find convenient gas stations</span>
+    <span className="splash-info">Know where the food is</span>
+    <span className="splash-info">Have a better idea of where to find lodging</span>
+    <span className="splash-info">The open road awaits!</span>
+ </div>   
+);
+
+const SplashInstructions = () => (
+  <div className="splash-container">
+    <h1 className="splash-title"> How To Use</h1>
+    <span className="splash-info">1) </span>
+    <span className="splash-info"> </span>
+    <span className="splash-info">Have a better idea of where to find lodging</span>
+    <span className="splash-info">The open road awaits!</span>
+  </div>
+
+);
+//clickcount % length
 class Splash extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      clickCount: 0
+    }
+    this.components = [<SplashInstructions/>, <SplashWelcome/>];
+    this.carousel = this.carousel.bind(this);
+  }
+
+  carousel(){
+    this.setState({ clickCount: this.state.clickCount + 1 });
+  }
   render() {
     if (this.props.currentUser.id) {
       return (
@@ -15,13 +49,8 @@ class Splash extends React.Component {
     } else {
       return (
         <div className="splash-page">
-           <div className="splash-container">
-           <h1 className="splash-title"> Welcome to Wayfarer!</h1>
-             <span className="splash-info">Find convenient gas stations</span>
-             <span className="splash-info">Know where the food is</span>
-             <span className="splash-info">Have a better idea of where to find lodging</span>
-             <span className="splash-info">The open road awaits!</span>
-           </div>
+            {this.components[this.state.clickCount % this.components.length]}
+            <button className="splash-carousel-nav" onClick={this.carousel}>More</button>
         </div>
       );
     }
